0e98aa60fd72ea6850346aace5ca18e1b1b75002
uslsteen <shurygin0171@gmail.com>
2021-05-12 22:45:19 +0300

    useless deleted

--- acronis_task/travelling_salesman/README.md
+++
@@ -1,7 +0,0 @@
-# 2 level
-Program reads from stdin and writes to stdout.
-
-**USAGE**
-```bash
-./circ < FILE
-```
---
+++ acronis_task/travelling_salesman/cmake-build-debug/CMakeFiles/Travelleri.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Travelleri.dir/algo/algorithm.cc.o"
+  "CMakeFiles/Travelleri.dir/circuits/circuits.cc.o"
+  "CMakeFiles/Travelleri.dir/compiler.tab.cc.o"
+  "CMakeFiles/Travelleri.dir/driver/driver.cc.o"
+  "CMakeFiles/Travelleri.dir/lex.yy.cc.o"
+  "CMakeFiles/Travelleri.dir/main.cc.o"
+  "CMakeFiles/Travelleri.dir/matrix/matrix.cc.o"
+  "CMakeFiles/Travelleri.dir/parser/parser.cc.o"
+  "Travelleri"
+  "Travelleri.pdb"
+  "compiler.tab.cc"
+  "compiler.tab.hh"
+  "lex.yy.cc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Travelleri.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
